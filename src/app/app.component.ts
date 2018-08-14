266fec27474c8b39d1d2dfda06f54b3d50840af2
Cheshire Cat <thacheshirecat@gmail.com>
2018-08-13 22:24:53 -0700

    Add code from homework.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { Task } from './models/task.model';
 
-@Component({
-  selector: 'app-root',
+@Component(
+{
+  selector: 'to-do-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent
+{
+  currentFocus: string = 'Angular Homework';
+  tasks: Task[] =
+  [
+    new Task('Finish Angular homework for Epicodus course'),
+    new Task('Level up my guy'),
+    new Task('Eat food (again)')
+  ];
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth() + 1;
+  day: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
 }
